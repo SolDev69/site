d1f34aa8fdcb8d58fb53092250f34c904a956925
SolDev <michaelcraft1104@gmail.com>
2025-05-15 17:11:43 -0400

    only prerender /cursedcreations for now

--- svelte.config.js
+++ svelte.config.js
@@ -23,4 +23,14 @@ const config = {
 
 config.paths = { base: process.argv.includes("dev") ? "" : process.env.BASE_PATH };
 
-export default config;
+export default {
+  kit: {
+    adapter: adapter(),
+    prerender: {
+      entries: [
+        '/cursedcreations' // 👈 only prerender this route
+      ]
+    }
+  }
+};
+
