d8f5f298db441a07245923b58114c35c3386e571
SolDev <michaelcraft1104@gmail.com>
2025-05-15 16:50:54 -0400

    move cc stuff to it's own path

--- src/routes/cursedcreations/downloads/expandedworlds/+page.svelte
+++ src/routes/cursedcreations/downloads/expandedworlds/+page.svelte
@@ -12,7 +12,7 @@
 
 <h2>How to Install:</h2>
 <p>
-    <a href="/wiki">See the wiki</a>
+    <a href="/cursedcreations/wiki">See the wiki</a>
 </p>
 
 <!-- I commented this cuz we don't really need this part -->
@@ -23,24 +23,24 @@
 <p class="note">Note from Zeta: This mods is in ALPHA and is likely very buggy. I don't even recall if I ever added proper 64bit worldgen to this mod but I'm uploading it regardless. This mod is also unsupported, as in it won't get any bugfixes for the time being.</p>
 <ul>
     <li>
-        <a href="/mods/64bit/infdev/inf-20100327/Zetas_64bit_inf-20100327_ALPHA_build_1.zip">ALPHA build 1</a>
+        <a href="/cursedcreations/mods/64bit/infdev/inf-20100327/Zetas_64bit_inf-20100327_ALPHA_build_1.zip">ALPHA build 1</a>
     </li>
     <li>
-        <a href="/mods/64bit/infdev/inf-20100327/Zetas_64bit_inf-20100327_ALPHA_build_2.zip">ALPHA build 2</a>
+        <a href="/cursedcreations/mods/64bit/infdev/inf-20100327/Zetas_64bit_inf-20100327_ALPHA_build_2.zip">ALPHA build 2</a>
     </li>
 </ul>
 
 <h3>Infdev 20100330 (Zero)</h3>
 <ul>
     <li>
-        <a href="/mods/64bit/infdev/inf-20100330/minecraft-inf-20100330-v1-zero.zip">v1.0</a>
+        <a href="/cursedcreations/mods/64bit/infdev/inf-20100330/minecraft-inf-20100330-v1-zero.zip">v1.0</a>
     </li>
 </ul>
 
 <h3>Infdev 20100420 (Zero)</h3>
 <ul>
     <li>
-        <a href="/mods/64bit/infdev/inf-20100420/minecraft-inf-20100420-v1-zero.zip">v1.0 (Zero)</a>
+        <a href="/cursedcreations/mods/64bit/infdev/inf-20100420/minecraft-inf-20100420-v1-zero.zip">v1.0 (Zero)</a>
     </li>
 </ul>
 
@@ -51,31 +51,31 @@
 </p>
 <ul>
     <li>
-        <a href="/mods/64bit/alpha/a1.0.1_01/ZZ-64bit_alpha_v1.0.1_01-0.0.1.zip">v0.0.1</a>
+        <a href="/cursedcreations/mods/64bit/alpha/a1.0.1_01/ZZ-64bit_alpha_v1.0.1_01-0.0.1.zip">v0.0.1</a>
     </li>
     <li>v0.0.2 (LOST)</li>
     <li>
-        <a href="/mods/64bit/alpha/a1.0.1_01/ZZ-64bit_alpha_v1.0.1_01-0.0.2_01.zip">v0.0.2_01</a>
+        <a href="/cursedcreations/mods/64bit/alpha/a1.0.1_01/ZZ-64bit_alpha_v1.0.1_01-0.0.2_01.zip">v0.0.2_01</a>
     </li>
     <li>
-        <a href="/mods/64bit/alpha/a1.0.1_01/ZZ-64bit_alpha_v1.0.1_01-0.0.2_02.zip">v0.0.2_02</a>
+        <a href="/cursedcreations/mods/64bit/alpha/a1.0.1_01/ZZ-64bit_alpha_v1.0.1_01-0.0.2_02.zip">v0.0.2_02</a>
     </li>
     <li>
-        <a href="/mods/64bit/alpha/a1.0.1_01/ZZ-64bit_alpha_v1.0.1_01-0.0.3.zip">v0.0.3</a>
+        <a href="/cursedcreations/mods/64bit/alpha/a1.0.1_01/ZZ-64bit_alpha_v1.0.1_01-0.0.3.zip">v0.0.3</a>
     </li>
     <li>v0.0.3_01 (LOST)</li>
     <li>
-        <a href="/mods/64bit/alpha/a1.0.1_01/ZZ-64bit_alpha_v1.0.1_01-0.0.3_02.zip">v0.0.3_02</a>
+        <a href="/cursedcreations/mods/64bit/alpha/a1.0.1_01/ZZ-64bit_alpha_v1.0.1_01-0.0.3_02.zip">v0.0.3_02</a>
     </li>
 </ul>
 
 <h3>Alpha v1.1.2_01 (Zero)</h3>
 <ul>
     <li>
-        <a href="/mods/64bit/alpha/a1.1.2_01/minecraft-a1.1.2_01-v1-zero.zip">v1.0 (Zero) (Client)</a>
+        <a href="/cursedcreations/mods/64bit/alpha/a1.1.2_01/minecraft-a1.1.2_01-v1-zero.zip">v1.0 (Zero) (Client)</a>
     </li>
     <li>
-        <a href="/mods/64bit/alpha/a1.1.2_01/minecraft_server-a1.1.2_01-v1-zero.zip">v1.0 (Zero) (Server)</a>
+        <a href="/cursedcreations/mods/64bit/alpha/a1.1.2_01/minecraft_server-a1.1.2_01-v1-zero.zip">v1.0 (Zero) (Server)</a>
     </li>
 </ul>
 
@@ -83,14 +83,14 @@
 <h3>Beta 1.1_02 (Zero)</h3>
 <ul>
     <li>
-        <a href="/mods/64bit/beta/b1.1_02/minecraft-b1.1_02-v1-zero.zip">v1.0</a>
+        <a href="/cursedcreations/mods/64bit/beta/b1.1_02/minecraft-b1.1_02-v1-zero.zip">v1.0</a>
     </li>
 </ul>
 
 <h3>Beta 1.7.3 (Zero)</h3>
 <ul>
     <li>
-        <a href="/mods/64bit/beta/b1.7.3/minecraft-b1.7.3-v1-zero.zip">v1.0 (Zero)</a>
+        <a href="/cursedcreations/mods/64bit/beta/b1.7.3/minecraft-b1.7.3-v1-zero.zip">v1.0 (Zero)</a>
     </li>
 </ul>
 
@@ -98,20 +98,20 @@
 <h3>1.5.2 (Zero)</h3>
 <ul>
     <li>
-        <a href="/mods/64bit/release/1.5.2/minecraft-1.5.2-64bit-v1-zero.zip">v1.0</a>
+        <a href="/cursedcreations/mods/64bit/release/1.5.2/minecraft-1.5.2-64bit-v1-zero.zip">v1.0</a>
     </li>
     <li>
-        <a href="/mods/64bit/release/1.5.2/minecraft-1.5.2-64bit-v1.1-zero.zip">v1.1</a>
+        <a href="/cursedcreations/mods/64bit/release/1.5.2/minecraft-1.5.2-64bit-v1.1-zero.zip">v1.1</a>
     </li>
     <li>
-        <a href="/mods/64bit/release/1.5.2/minecraft-1.5.2-64bit-v1.2-zero.zip">v1.2</a>
+        <a href="/cursedcreations/mods/64bit/release/1.5.2/minecraft-1.5.2-64bit-v1.2-zero.zip">v1.2</a>
     </li>
 </ul>
 
 <h3>1.6.4 (Zero)</h3>
 <ul>
     <li>
-        <a href="/mods/64bit/release/1.6.4/minecraft-1.6.4-64bit-v1-zero.zip">v1.0</a>
+        <a href="/cursedcreations/mods/64bit/release/1.6.4/minecraft-1.6.4-64bit-v1-zero.zip">v1.0</a>
     </li>
 </ul>
 
@@ -124,49 +124,49 @@
 <br />
 <ul>
     <li>
-        <a href="/mods/64bit/release/1.8/minecraft-1.8-64bit-v1-zero.zip">v1.0</a>
+        <a href="/cursedcreations/mods/64bit/release/1.8/minecraft-1.8-64bit-v1-zero.zip">v1.0</a>
     </li>
     <li>
-        <a href="/mods/64bit/release/1.8/minecraft-1.8-64bit-v2-zero.zip">v2.0</a>
+        <a href="/cursedcreations/mods/64bit/release/1.8/minecraft-1.8-64bit-v2-zero.zip">v2.0</a>
     </li>
     <li>
-        <a href="/mods/64bit/release/1.8/minecraft-1.8-64bit-v3-zero.zip">v3.0</a>
+        <a href="/cursedcreations/mods/64bit/release/1.8/minecraft-1.8-64bit-v3-zero.zip">v3.0</a>
     </li>
 </ul>
 <h3>1.9 (Zero)</h3>
 <ul>
     <li>
-        <a href="/mods/64bit/release/1.9/minecraft-1.9-64bit-v1-zero.zip">v1.0</a>
+        <a href="/cursedcreations/mods/64bit/release/1.9/minecraft-1.9-64bit-v1-zero.zip">v1.0</a>
     </li>
 </ul>
 <h3>1.10.2 (Zero)</h3>
 <ul>
     <li>
-        <a href="/mods/64bit/release/1.10.2/minecraft-1.10.2-64bit-v1-zero.zip">v1.0</a>
+        <a href="/cursedcreations/mods/64bit/release/1.10.2/minecraft-1.10.2-64bit-v1-zero.zip">v1.0</a>
     </li>
 </ul>
 <h3>1.11.2 (Zero)</h3>
 <ul>
     <li>
-        <a href="/mods/64bit/release/1.11.2/minecraft-1.11.2-64bit-v1-zero.zip">v1.0</a>
+        <a href="/cursedcreations/mods/64bit/release/1.11.2/minecraft-1.11.2-64bit-v1-zero.zip">v1.0</a>
     </li>
 </ul>
 <h3>1.12.2 (Zero)</h3>
 <ul>
     <li>
-        <a href="/mods/64bit/release/1.12.2/minecraft1.12.2-64bit-V1.zip">v1.0</a>
+        <a href="/cursedcreations/mods/64bit/release/1.12.2/minecraft1.12.2-64bit-V1.zip">v1.0</a>
     </li>
     <li>
-        <a href="/mods/64bit/release/1.12.2/minecraft1.12.2-64-bit-V2.zip">v2.0</a>
+        <a href="/cursedcreations/mods/64bit/release/1.12.2/minecraft1.12.2-64-bit-V2.zip">v2.0</a>
     </li>
     <li>
-        <a href="/mods/64bit/release/1.12.2/minecraft-1.12.2-64bit-v2.5-zero.zip">v2.5</a>
+        <a href="/cursedcreations/mods/64bit/release/1.12.2/minecraft-1.12.2-64bit-v2.5-zero.zip">v2.5</a>
     </li>
     <li>
-        <a href="/mods/64bit/release/1.12.2/minecraft-1.12.2-64bit-v2.75.zip">v2.75</a>
+        <a href="/cursedcreations/mods/64bit/release/1.12.2/minecraft-1.12.2-64bit-v2.75.zip">v2.75</a>
     </li>
     <li>
-        <a href="/mods/64bit/release/1.12.2/minecraft-1.12.2-64bit-v3.zip">v3.0</a>
+        <a href="/cursedcreations/mods/64bit/release/1.12.2/minecraft-1.12.2-64bit-v3.zip">v3.0</a>
     </li>
 </ul>
 <h3>mclong 1.19.2-1.20.1 (AlphaMode)</h3>
@@ -179,7 +179,7 @@
 <h3>1.20.1 (AlphaMode)</h3>
 <ul>
     <li>
-        <a href="/mods/unlimited/release/1.20.1/mclong-1.20.1-0.0.0-1.20.1-big-installer.jar">Installer (unknown version)</li>
+        <a href="/cursedcreations/mods/unlimited/release/1.20.1/mclong-1.20.1-0.0.0-1.20.1-big-installer.jar">Installer (unknown version)</li>
     <li>
         <a href="https://github.com/AlphaMode/MinecraftLong2/tree/1.20.1-big">Source</a>
     </li>
